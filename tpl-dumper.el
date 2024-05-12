b06f57ef04e213ea521e2ddd7b404249b5eb0804
Nate Nichols <nathannichols454@gmail.com>
2024-05-12 09:20:26 -0400

    implemented / refined project template trees

--- tpl-dumper.el
+++ tpl-dumper.el
@@ -3,7 +3,7 @@
 ;; Author: Nathan Nichols
 ;; Maintainer: Nathan Nichols
 ;; Version: 1.0
-;; Package-Requires: ((emacs "28.1") (yasnippet "0.14.0"))
+;; Package-Requires: ((emacs "24.1") (cl-lib "0.7.1") (yasnippet "0.14.0") (f "0.20.0"))
 ;; Package
 ;; Homepage: https://resultsmotivated.com/
 ;; Keywords: yasnippet templating convenience tools
@@ -38,7 +38,7 @@
 
 (defun tpl-dumper-insert-yas-by-name (name)
   "Insert Yas-snippet NAME at point."
-  (flet ((dummy-prompt
+  (cl-flet ((dummy-prompt
           (prompt choices &optional display-fn)
           (declare (ignore prompt))
           (or (find name choices :key display-fn :test #'string=)
@@ -67,7 +67,7 @@
 CALLBACK is a string or a function that returns a string."
   (tpl-dumper-mkdir-p outdir)
   (let* ((ts (tpl-dumper-str-or-fn-callback callback))
-         (fpath (file-name-concat outdir (concat ts ext))))
+         (fpath (f-join outdir (concat ts ext))))
     (find-file fpath)
     (message fpath)))
 
@@ -83,6 +83,135 @@ Return the path of the newly-created file."
     (tpl-dumper-insert-yas-by-name yas-tpl)
     new-file))
 
+
+;; ###################################################################
+;; Functionality for creating projects from templates
+;; ###################################################################
+
+(defun tpl-dumper-dump-simple (file-path tpl-name)
+  "Create file FILE-PATH from yas template TPL-NAME."
+  (let ((file-contents
+         (with-temp-buffer
+           (yas-minor-mode 1)
+           (tpl-dumper-insert-yas-by-name tpl-name)
+           (buffer-substring-no-properties (point-min) (point-max)))))
+    (f-write-text file-contents 'utf-8 file-path)))
+
+
+(defun tpl-dumper-mk-file-tpl (file-path tpl-name)
+  "Create file FILE-PATH from Yasnippet template TPL-NAME.
+Does not overwrite existing files and creates missing parent
+directories as neccessary."
+  (if (f-exists-p file-path)
+      (message
+       (format "[tpl-dumper] File '%s' already exists, skipping..."
+               file-path))
+    (progn
+      (message (format
+                "[tpl-dumper] Making file '%s' from template '%s'"
+                file-path
+                tpl-name))
+      (tpl-dumper-mkdir-p (f-dirname file-path))
+      (tpl-dumper-dump-simple file-path tpl-name))))
+
+;; ###################################################################
+;; These functions determine the metadata/structure of a file-spec.
+;; ###################################################################
+
+(defun tpl-dumper-is-file-spec (tree)
+  "Whether TREE is a file-spec."
+  (and (listp tree)
+       (eql 'file (car tree))
+       (cl-every #'stringp (cdr tree))))
+
+(defun tpl-dumper-dump-file-spec (spec)
+  "Create a file from a file-spec SPEC."
+  (let* ((abs-path (nth 1 spec))
+         (tpl-name (nth 2 spec)))
+    (tpl-dumper-mk-file-tpl abs-path tpl-name)))
+
+(defun tpl-dumper--mk-rel-file (basepath file-spec)
+  "Make file-spec FILE-SPEC relative to BASEPATH."
+  (let* ((path-orig (nth 1 file-spec))
+        (new-path (f-join basepath path-orig)))
+    (cons 'file (cons new-path (cdr (cdr file-spec))))))
+
+;; ###################################################################
+;; These two functions are not used internally, the struture of a
+;; path-spec is determined by `tpl-dumper-mk-tree'.
+;; ###################################################################
+
+(defun tpl-dumper-is-path-spec (tree)
+  "Whether TREE is a path-spec.
+A directory spec is a list starting with the symbol `dir'
+followed by a string (the path) and then 0 or more lists."
+  (and (listp tree)
+       (eql 'dir (car tree))
+       (stringp (car (cdr tree)))
+       (cl-every #'listp (cdr (cdr tree)))))
+
+(defun tpl-dumper-dump-path-spec (spec)
+  "Create a directory from a path-spec SPEC."
+  (let* ((abs-path (nth 1 spec))
+         (tpl-name (nth 2 spec)))
+    (tpl-dumper-mk-file-tpl abs-path tpl-name)))
+
+(defun tpl-dumper--mk-rel-path (basepath path-spec)
+  "Make path-spec PATH-SPEC relative to BASEPATH."
+  (let* ((path-orig (car (cdr path-spec)))
+        (new-path (f-join basepath path-orig)))
+    (cons 'dir (cons new-path (cdr (cdr path-spec))))))
+
+;; ###################################################################
+
+(defun tpl-dumper-mk-rel (basepath spec)
+  "Make SPEC relative to BASEPATH.
+SPEC is either a file-spec or a path-spec."
+  (cond ((tpl-dumper-is-file-spec spec)
+         (tpl-dumper--mk-rel-file basepath spec))
+        ((tpl-dumper-is-path-spec spec)
+         (tpl-dumper--mk-rel-path basepath spec))
+        (t
+         (error "[tpl-dumper] Expected a path-spec or a file-spec"))))
+
+(defun tpl-dumper-mk-tree (tree)
+  "Create a project from a project template spec TREE."
+  (interactive)
+  (cond ((tpl-dumper-is-file-spec tree)
+         (tpl-dumper-dump-file-spec tree))
+        ((tpl-dumper-is-path-spec tree)
+         (let* ((basepath (car (cdr tree)))
+                (tree0 (car (cdr (cdr tree)))))
+           (dolist (item tree0)
+             ;; Update any paths to be relative to `basepath' then
+             ;; recurse on each directory item
+             (tpl-dumper-mk-tree
+              (tpl-dumper-mk-rel basepath item)))))
+        (t
+         (error "[tpl-dumper] Expected a path-spec or a file-spec"))))
+
+(defun tpl-dumper-mk-proj-tree-rel (tree)
+  "Create project from template tree TREE.
+User is prompted for the directory under which the template tree is created."
+  (interactive)
+  (let* ((path (read-directory-name "Enter directory:"))
+         (tree-rel (tpl-dumper-mk-rel path tree)))
+    (tpl-dumper-mk-tree tree-rel)))
+
+;; ###################################################################
+;; Extremely basic self-tests
+;; ###################################################################
+
+(let ((fspec '(file "README.org" "org-default-readme")))
+  (cl-assert (tpl-dumper-is-file-spec fspec)))
+
+
+(let ((tree-py-proj '(dir "/tmp/testproj/"
+                          ((file "main.py" "python-default-file")
+                           (file "README.org" "org-default-readme")
+                           (file "LICENSE" "license-expat")
+                           (file ".gitignore" "gitignore-default-file")))))
+  (cl-assert (tpl-dumper-is-path-spec tree-py-proj)))
 
 (provide 'tpl-dumper)
 ;;; tpl-dumper.el ends here
